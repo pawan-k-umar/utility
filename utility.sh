db6c1e5fdf4720af80e7e568b67f271cfb89fd65
Pawan Pumar <itsmekr.info@gmail.com>
2025-06-07 18:27:34 +0530

    added shel script to install required software

--- utility.sh
+++ utility.sh
@@ -70,10 +70,12 @@ case "$OS" in
 
     install_package "Docker" "command -v docker" "sudo yum install -y docker && sudo systemctl enable --now docker"
 
-    install_package "Jenkins" "command -v jenkins" "
-      sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo &&
-      sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key &&
-      sudo yum install -y java-17-openjdk jenkins
+   install_package "Jenkins" "command -v jenkins" "
+    sudo rm -f /etc/apt/sources.list.d/jenkins.list /usr/share/keyrings/jenkins-keyring.gpg &&
+    curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | gpg --dearmor | sudo tee /usr/share/keyrings/jenkins-keyring.gpg > /dev/null &&
+    echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null &&
+    sudo apt-get update -y &&
+    sudo apt-get install -y openjdk-17-jdk jenkins
     "
 
     install_package "Maven" "command -v mvn" "sudo yum install -y maven"
